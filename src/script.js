85dc4d77728b761f5eda643e285a24b34dada135
Alongedaniel <alongedaniel41@gmail.com>
2023-09-06 09:33:10 +0100

    All keys are clickable and the input gets updated

---
+++ src/script.js
@@ -0,0 +1,34 @@
+'use strict'
+const numberKeys = document.querySelectorAll('.dark-btn')
+const input = document.querySelector('.input')
+const output = document.querySelector('.output')
+const actions = document.querySelectorAll('[data-action]')
+const deleteBtn = document.querySelector('.delete')
+const clear = document.querySelector('.clear')
+const equalButton = document.querySelector('.equal')
+
+numberKeys.forEach(numberKey => {
+    numberKey.addEventListener('click', () => {
+        input.innerHTML += numberKey.innerHTML
+    })
+})
+
+actions.forEach(action => {
+    action.addEventListener('click', () => {
+        input.innerHTML += action.innerHTML
+    })
+})
+
+deleteBtn.addEventListener('click', () => {
+    input.innerText = input.innerText.slice(0, -1);
+})
+
+clear.addEventListener('click', () => {
+    input.innerText = '';
+    output.innerText = '';
+})
+
+
+
+
+
